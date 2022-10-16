f2e2a4a2590671fd7c103d448bea542740c1f8ee
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-10-16 23:46:09 +0200

    Add files via upload

---
+++ Inicijalno-Osmi/Inicijalno-Osmi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_Osmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int br1 = 0;
+            int br2 = 1;
+            int fBr = 0;
+            int n;
+
+            Console.WriteLine("Upišite broj n:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i<n; i++)
+            {
+                fBr=br1+br2;
+
+                br1=br2;
+                br2=fBr;
+            }
+
+            Console.WriteLine(fBr);
+            Console.ReadKey();
+        }
+    }
+}
